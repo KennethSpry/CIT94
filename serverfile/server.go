7c63c3e5eac4f76299b7daed53af8780ae590283
KennethSpry <0162268@my.scccd.edu>
2017-03-03 10:09:25 -0800

    Update server.go

--- serverfile/server.go
+++ serverfile/server.go
@@ -6,15 +6,15 @@ import (
 )
 func main() {
   http.HandleFunc("/", tiger)
-  http.HandleFunc("/tiger_1.jpg", tigerpic)
+  http.HandleFunc("/tiger_1.jpeg", tigerpic)
   http.ListenAndServe(":8080", nil)
 }
 
 func tiger(w http.ResponseWriter, req *http.Request){
   w.Header().Set("Content-Type" ,"text/html; charset=utf-8")
-  io.WriteString(w, `<img scr="tiger_1.jpg"> `)
+  io.WriteString(w, `<img scr="tiger_1.jpeg"> `)
 }
 
 func tigerPic(w http.ResponseWriter, req *http.Request) {
-  http.ServerFile(w, req, "serverfile/img/tiger_1.jpg")
+  http.ServerFile(w, req, "serverfile/img/tiger_1.jpeg")
 }
