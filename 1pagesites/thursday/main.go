678b2b0ac97f486934dd4cc9d81430c940515bf9
KennethSpry <0162268@my.scccd.edu>
2017-03-03 10:01:17 -0800

    Update main.go

--- 1pagesites/thursday/main.go
+++ 1pagesites/thursday/main.go
@@ -13,14 +13,15 @@ func main() {
 }
 func index(w http.ResponseWriter, req *http.request) {
 w.Header().Set("Content-Type": "text/html; charset=utf-8")
-io.WriteString(w, "template") 
+io.WriteString(w, req "template") 
 }  
 
 func css(w http.ResponseWriter, req *http.request) {
-http.ServerFile 
-  
+http.ServerFile(w, req "main.css") 
+}
 func template(w http.ResponseWriter, req *http.request) {
-http.ServerFile
-    
+http.ServerFile(w, req "imerge.jpeg")
+}    
 func imerge(w http.ResponseWriter, req *http.request) {
-http.ServerFile
+http.ServerFile(w, req "template")
+}
