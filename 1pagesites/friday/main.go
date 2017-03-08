08c2d8b88b258dab64b67b654d2ff73bd724b086
KennethSpry <0162268@my.scccd.edu>
2017-03-08 15:05:40 -0800

    Update main.go

--- 1pagesites/friday/main.go
+++ 1pagesites/friday/main.go
@@ -12,7 +12,7 @@ func init() {
 func main() {
   http.HandleFunc("/",index)
   http.HandleFunc("/main.css",css)
-  http.HandleFunc("cumulus_clouds_in_fair_weather",clouds)
+  http.HandleFunc("om5z3s-b88903094z.120170301173338000g43lq21q.10.jpeg",burgerandicecoldbeer)
   http.ListenAndServe(":8080", nil)
 }
 func index(w http.ResponseWriter, req *http.request) {
@@ -24,5 +24,5 @@ http.ServerFile(w, req, "main.css")
 }
                  
 func imerge(w http.ResponseWriter, req *http.request) {
-http.ServerFile(w, req, "Cumulus_clouds_in_fair_weather.jpeg",clouds)
+http.ServerFile(w, req, "om5z3s-b88903094z.120170301173338000g43lq21q.10.jpeg",burgerandicecoldbeer)
 }
