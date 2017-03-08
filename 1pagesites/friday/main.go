3e8373a1ef5fa83d4b70166418e6962635faf32a
KennethSpry <0162268@my.scccd.edu>
2017-03-08 14:56:51 -0800

    Update main.go

--- 1pagesites/friday/main.go
+++ 1pagesites/friday/main.go
@@ -1,1 +1,28 @@
+package main
 
+import(
+  "net/http"
+  "html/template"
+)
+var tpl *template.Template
+
+func init() {
+  tpl =template.Must(template.ParseGlob("templates/*"))
+  
+func main() {
+  http.HandleFunc("/",index)
+  http.HandleFunc("/main.css",css)
+  http.HandleFunc("cumulus_clouds_in_fair_weather",clouds)
+  http.ListenAndServe(":8080", nil)
+}
+func index(w http.ResponseWriter, req *http.request) {
+  tpl.ExecuteTemplate(w, "index.html", nil)
+}  
+
+func css(w http.ResponseWriter, req *http.request) {
+http.ServerFile(w, req, "main.css") 
+}
+                 
+func imerge(w http.ResponseWriter, req *http.request) {
+http.ServerFile(w, req, "Cumulus_clouds_in_fair_weather.jpeg",clouds)
+}
