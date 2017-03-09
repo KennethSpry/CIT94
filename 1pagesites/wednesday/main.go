5e01c33fa20607229e0d9b0cffe7aa744d73dac2
KennethSpry <0162268@my.scccd.edu>
2017-03-08 16:14:39 -0800

    Update main.go

--- 1pagesites/wednesday/main.go
+++ 1pagesites/wednesday/main.go
@@ -1,1 +1,28 @@
+package main
 
+import(
+  "net/http"
+  "html/template"
+)
+var tpl *template.Template
+
+func init() {
+  tpl =template.Must(template.ParseGlob("templates/*"))
+  
+func main() {
+  http.HandleFunc("/",index)
+  http.HandleFunc("/main.css",css)
+  http.HandleFunc("mountains.jpeg",escape)
+  http.ListenAndServe(":8080", nil)
+}
+func index(w http.ResponseWriter, req *http.request) {
+  tpl.ExecuteTemplate(w, "index.html", nil)
+}  
+
+func css(w http.ResponseWriter, req *http.request) {
+http.ServerFile(w, req, "main.css") 
+}
+                 
+func imerge(w http.ResponseWriter, req *http.request) {
+http.ServerFile(w, req, "mountains.jpeg",escape)
+}
