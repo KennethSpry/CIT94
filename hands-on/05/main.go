041bfd22aa14df8c27229c46e3cb59876024a903
KennethSpry <0162268@my.scccd.edu>
2017-04-07 12:22:15 -0700

    Update main.go

--- hands-on/05/main.go
+++ hands-on/05/main.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+  "net/http"
+  "html/template"
+)
+
+var tpl *template.Template
+
+func init()  {
+  tpl = template.Must(template.ParseFiles("templates/index.html"))
+}
+
+func main() {
+  fs := http.FileServer(http.Dir("public pics"))
+  http.Handle("/public pics/", fs)
+  http.HandleFunc("/", index)
+  http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "index.html", nil)
+}
