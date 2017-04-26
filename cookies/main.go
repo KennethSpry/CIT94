153e45ef5051b463675227e54cbf91eec5ed832c
KennethSpry <0162268@my.scccd.edu>
2017-04-26 09:15:57 -0700

    Update main.go

--- cookies/main.go
+++ cookies/main.go
@@ -25,3 +25,34 @@ func index(w http.ResponseWriter, r *http.Request) {
   }
   tpl.ExecuteTemplate(w, "index.html", c)
 }
+func bowzer(w http.ResponseWriter, r *http.Request) {
+  c := &http.Cookie{
+    Name: "User-Cookie",
+    Value: "Value",
+    Path: "/dog/bowzer",
+  }
+  http.SetCookie(w, c)
+  tpl.ExecuteTemplate(w, "bowzer.html", c)
+
+}
+
+func bowzerpics(w http.ResponseWriter, r *http.Request) {
+  var c *http.Cookie
+  c, err := r.Cookie("User-Cookie")
+  if err != nil {
+    fmt.Println(err)
+  }
+  tpl.ExecuteTemplate(w, "bowzerpics.html", c)
+
+
+}
+
+func cat(w http.ResponseWriter, r *http.Request) {
+  var c *http.Cookie
+  c, err := r.Cookie("User-Cookie")
+  if err != nil {
+    fmt.Println(err)
+  }
+  tpl.ExecuteTemplate(w, "cat.html", c)
+
+}
