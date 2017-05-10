80ee2ffe503a4b93dcd6cf1bafdf865251e3d3bc
KennethSpry <0162268@my.scccd.edu>
2017-05-10 12:44:56 -0700

    Update unmarshal.go

--- Json/unmarshal/unmarshal.go
+++ Json/unmarshal/unmarshal.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+  "encoding/json"
+  "fmt"
+)
+type player struct{
+  FirstName string
+  LastName string
+  UserName string
+  Age int
+  PremiumMember bool
+}
+
+func main() {
+
+  j := `[{"FirstName":"Kenneth","LastName":"Spry","UserName":"Shadowhawk","Age":55,"PremiumMember":false},{"FirstName":"Micheal","LastName":"Robby","UserName":"Elite H@xor","Age":23,"PremiumMember":true}]`
+
+  fmt.Println("JSON: ", j)
+
+  xpl := []player{}
+
+  err := json.Unmarshal([]byte(j), &xpl)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  fmt.Printf("Go: %+v \n", xpl)
+
+}
